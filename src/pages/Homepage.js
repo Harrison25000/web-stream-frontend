2f5c4c3b0153e66c63de056adc7865929222ac08
Oliver Harrison <ofcharrison@gmail.com>
2023-03-21 16:57:40 +0000

    homepage intro text

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -21,16 +21,26 @@ const Homepage = () => {
     }
 
     return (
-        <div className="HomepageDiv">
-            <div className="HomepageTab" onClick={() => tabClicked(Constants.WATCH)}>
-                <img src={watchIcon} alt="watch" id="watchIconImage"></img>
-                <h3 id="watchTabTitle">Watch</h3>
+        <div className='Homepage'>
+            <div className='HomepageText'>
+                <h3>About Web Stream</h3>
+                <p>
+                    Welcome to Web Stream, a live streaming service provider.<br />
+                    Simply click on the <b>Broadcast</b> section to go to the broadcasting page where you can live stream directly from your camera.<br />
+                    Alternatively tune in to your favourite streamers by clicking on the <b>Watch</b> section.
+                </p>
             </div>
-            <div className="HomepageTab" onClick={() => tabClicked(Constants.BROADCAST)}>
-                <img src={broadcastIcon} alt="broadcast" id="broadcastIconImage"></img>
-                <h3 id="broadcastTabTitle">Broadcast</h3>
-            </div>
-        </div >
+            <div className="HomepageDiv">
+                <div className="HomepageTab" onClick={() => tabClicked(Constants.WATCH)}>
+                    <img src={watchIcon} alt="watch" id="watchIconImage"></img>
+                    <h3 id="watchTabTitle">Watch</h3>
+                </div>
+                <div className="HomepageTab" onClick={() => tabClicked(Constants.BROADCAST)}>
+                    <img src={broadcastIcon} alt="broadcast" id="broadcastIconImage"></img>
+                    <h3 id="broadcastTabTitle">Broadcast</h3>
+                </div>
+            </div >
+        </div>
     )
 }
 
